b5849c9be7f4963342fb987a6bfd0e5ef8cc6f06
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-05-07 21:28:51 +1200

    Use a context manager for the file stream

--- UVAPIConversion.py
+++ UVAPIConversion.py
@@ -22,25 +22,22 @@ def get(url, params=None):
 #    data = json.loads(url.read().decode())
 #    print(data)
 
-f= open("/out/uvi-nzmet.csv","w+")
-    
-for i in range(len(towns)):
-  print(towns[i])
-  print(i)
-  response = get(baseurl, params={'apikey': apikey, 'lat':towns[i][2], 'long':towns[i][1]})
-  uvjson = response.json()
-  values = uvjson['products'][product]['values']
-  print(values)
-  f.write(towns[i][0] +"\r\n")
-  for i in range(len(values)):
-    print(values[i]['time'])
-    f.write(values[i]['time']+",")
-    
-  f.write("\r\n")
-  for i in range(len(values)):
-    print(values[i]['value'])
-    f.write(str(values[i]['value'])+",")
-  f.write("\r\n")
-  f.write("\r\n")
-  
-f.close()  
+with open("/out/uvi-nzmet.csv","w+") as f:
+  for i in range(len(towns)):
+    print(towns[i])
+    print(i)
+    response = get(baseurl, params={'apikey': apikey, 'lat':towns[i][2], 'long':towns[i][1]})
+    uvjson = response.json()
+    values = uvjson['products'][product]['values']
+    print(values)
+    f.write(towns[i][0] +"\r\n")
+    for i in range(len(values)):
+      print(values[i]['time'])
+      f.write(values[i]['time']+",")
+
+    f.write("\r\n")
+    for i in range(len(values)):
+      print(values[i]['value'])
+      f.write(str(values[i]['value'])+",")
+    f.write("\r\n")
+    f.write("\r\n")
